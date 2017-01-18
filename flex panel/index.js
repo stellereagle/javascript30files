711a5ec6588e3671ba350f69e6c2c12582a793ee
stellereagle <solai@hotmail.co.uk>
2017-01-18 10:27:58 +0530

    Flex pannel finished

--- flex panel/index.js
+++ flex panel/index.js
@@ -3,8 +3,18 @@ function randomGenerator () {
 	date = date.getMilliseconds()
 	return chooseImage = Math.round(Math.random() * (date - 1) + 1);
 }
+function toggleClass () {
+	this.classList.toggle("open")
+}
+function toggleActive (e) {
+	if(e.propertyName.includes("flex")){
+		this.classList.toggle("open-active")
+	}
+}
 const panels = document.querySelectorAll('.panel');
 panels.forEach(panel => {
-	let chooseImage = randomGenerator();
+	let chooseImage = randomGenerator()
 	panel.style.backgroundImage = "url('https://unsplash.it/1000?image="+chooseImage+"')"
-});
+})
+panels.forEach(panel => panel.addEventListener("click", toggleClass))
+panels.forEach(panel => panel.addEventListener("transitionend", toggleActive))
