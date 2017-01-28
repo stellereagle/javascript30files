fb968dd01284eae54eacc5f7f3cd28b4d0caba19
stellereagle <solai@hotmail.co.uk>
2017-01-28 22:04:44 +0530

    Final ajax type ahead

--- Ajax type/index.js
+++ Ajax type/index.js
@@ -11,3 +11,32 @@ function findMatches (wordToMatch, cities) {
     return place.city.match(regex)
   })
 }
+
+function numberWithCommas (x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
+function displayMatches () {
+  if (this.value === '') {
+    suggestions.innerHTML = ' '
+    return false
+  }
+  const matchArray = findMatches(this.value, cities)
+  const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi')
+    const cityName = place.city.replace(regex, `<span class="h1">${this.value}</span>`)
+    const stateName = place.state.replace(regex, `<span class="h1">${this.value}</span>`)
+    return `<li>
+      <span class="name">${cityName}</span>
+      <span class="name">${stateName}</span>
+      <span class="population">${numberWithCommas(place.population)}</span>
+      </li>`
+  }).join('')
+  suggestions.innerHTML = html
+}
+
+const searchInput = document.querySelector('.search')
+const suggestions = document.querySelector('.suggestions')
+
+searchInput.addEventListener('change', displayMatches)
+searchInput.addEventListener('keyup', displayMatches)
