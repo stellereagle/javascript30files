85274ce169bd3d45348c75a66ea33996a1b7fb7f
stellereagle <solai@hotmail.co.uk>
2017-02-04 22:24:03 +0530

    Standard js and few more lines added

--- 11 HTML Player/app.js
+++ 11 HTML Player/app.js
@@ -1,37 +1,42 @@
 const dom = document
-const player = dom.querySelector(".player")
-const viewer = player.querySelector(".viewer")
-const progress = player.querySelector(".progress")
-const progressBar = player.querySelector(".progressFilled")
-const playerButton = player.querySelector(".playerButton")
-const skipButtons = player.querySelectorAll("[data-skip")
-const ranges = player.querySelectorAll(".playerSlider")
+const player = dom.querySelector('.player')
+const viewer = player.querySelector('.viewer')
+const progress = player.querySelector('.progress')
+const progressBar = player.querySelector('.progressFilled')
+const playerButton = player.querySelector('.playerButton')
+const skipButtons = player.querySelectorAll('[data-skip')
+const ranges = player.querySelectorAll('.playerSlider')
 
 function togglePlay () {
-	const method = viewer.paused ? "play" : "pause" ;
-	viewer[method]();
+  const method = viewer.paused ? 'play' : 'pause'
+  viewer[method]()
 }
 function updateButton () {
-	const icon = viewer.paused ? "⏯" : "⏸"
-	playerButton.textContent = icon
+  const icon = viewer.paused ? '⏯' : '⏸'
+  playerButton.textContent = icon
 }
 
-function skip() {
-	viewer.currentTime += parseFloat(this.dataset.skip)
+function skip () {
+  viewer.currentTime += parseFloat(this.dataset.skip)
+}
+
+function handleProgress () {
+  const percent = (viewer.currentTime / viewer.duration) * 100
+  progressBar.style.flexBasis = `${percent}%`
 }
 
 function handleRangeUpdate () {
-	viewer[this.name] = this.value;
-	console.log(this.name)
-	console.log(this.value)
+  viewer[this.name] = this.value
+  console.log(this.name)
+  console.log(this.value)
 }
 
-viewer.addEventListener("click", togglePlay);
-viewer.addEventListener("play", updateButton);
-viewer.addEventListener("pause", updateButton);
+viewer.addEventListener('click', togglePlay)
+viewer.addEventListener('play', updateButton)
+viewer.addEventListener('pause', updateButton)
 
-playerButton.addEventListener("click", togglePlay);
+playerButton.addEventListener('click', togglePlay)
 
-skipButtons.forEach(button => button.addEventListener("click", skip))
+skipButtons.forEach(button => button.addEventListener('click', skip))
 
-ranges.forEach(range => range.addEventListener("click", handleRangeUpdate))
+ranges.forEach(range => range.addEventListener('click', handleRangeUpdate))
