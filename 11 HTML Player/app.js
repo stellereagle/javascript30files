3b9cb311bb9290113f929ee0d58e3b6b85f13646
stellereagle <solai@hotmail.co.uk>
2017-02-04 22:12:27 +0530

    First iteration html player

---
+++ 11 HTML Player/app.js
@@ -0,0 +1,37 @@
+const dom = document
+const player = dom.querySelector(".player")
+const viewer = player.querySelector(".viewer")
+const progress = player.querySelector(".progress")
+const progressBar = player.querySelector(".progressFilled")
+const playerButton = player.querySelector(".playerButton")
+const skipButtons = player.querySelectorAll("[data-skip")
+const ranges = player.querySelectorAll(".playerSlider")
+
+function togglePlay () {
+	const method = viewer.paused ? "play" : "pause" ;
+	viewer[method]();
+}
+function updateButton () {
+	const icon = viewer.paused ? "⏯" : "⏸"
+	playerButton.textContent = icon
+}
+
+function skip() {
+	viewer.currentTime += parseFloat(this.dataset.skip)
+}
+
+function handleRangeUpdate () {
+	viewer[this.name] = this.value;
+	console.log(this.name)
+	console.log(this.value)
+}
+
+viewer.addEventListener("click", togglePlay);
+viewer.addEventListener("play", updateButton);
+viewer.addEventListener("pause", updateButton);
+
+playerButton.addEventListener("click", togglePlay);
+
+skipButtons.forEach(button => button.addEventListener("click", skip))
+
+ranges.forEach(range => range.addEventListener("click", handleRangeUpdate))
